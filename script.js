bf44ef3538b9671d93393ad53ab65a1cd55a4dea
AgentPika <gluardalan@gmail.com>
2020-10-21 19:22:56 +0000

    Add generator and change file

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-alert("Fait par Pika#8509 Game Version: 1.1.1")
+alert("The game is in development. Game version: 1.1.2");
 var x=0;
 var lv=1;
 var at=1;
+var at2=1;
 function oncl(){
-   x+=lv;
+   x+=lv;  
    textUpdate();
     
 }
@@ -17,22 +18,45 @@ function autoclick(){
 		case 1:
 		if (x>=150){
 			at++;
-			x-=150;  document.getElementById("g").innerHTML= "cost:250| Upgrade";
+			x-=150;  document.getElementById("g").innerHTML= "cost:250| Generator lv: 2";
 			setInterval(oncl, 1000);
 		}
 		break;
 		case 2:
 		if (x>=250){
 			at=3;
-			x-=200;  document.getElementById("g").innerHTML= "cost:550| Upgrade";
+			x-=200;  document.getElementById("g").innerHTML= "cost:550| Generator lv: 3";
 		}
 		break;
 		case 3:
 		if (x>=550){
 			at=5;
-			x-=550;  document.getElementById("g").innerHTML= "cost:??| Fully Upgrade";
+			x-=550;  document.getElementById("g").innerHTML= "cost:??| Fully Upgrade lv: 4";
 		}
 		break;
+	}
+}
+
+function autoclick2(){
+	switch (at2) {
+		case 1:
+		if (x>=300){
+			at2=3;
+			x-=300;  document.getElementById("g2").innerHTML= "cost: 450| Generator 2  lv: 2"
+			setInterval(oncl, 1000);
+		}
+		break;
+		case 2:
+		if (x>=450){
+			at2=4;
+			x-=450;  document.getElementById("g2").innerHTML= "cost: 700| Generator 2 lv: 3"
+		}
+		break;
+		case 3:
+		if (x>=700){
+			at2=6;
+			x-=700;  document.getElementById("g2").innerHTML= "cost: ??| Fully Upgrade lv: 4"
+		}
 	}
 }
 
@@ -41,21 +65,21 @@ function upgrade(){
       case 1:
       if (x>=100){
          lv++;
-         x-=100;   document.getElementById("u").innerHTML= "cost: 200| Upgrade";
+         x-=100;   document.getElementById("u").innerHTML= "cost: 200| Upgrade lv: 2";
       }
      break;
      case 2:
    
    if (x>=200){
          lv=3;
-         x-=200;   document.getElementById("u").innerHTML= "cost: 500| Upgrade";
+         x-=200;   document.getElementById("u").innerHTML= "cost: 500| Upgrade lv: 3";
 }
       break;
 	  case 3:
 
 	if (x>=500){
 		lv=5;
-		x-=500;    document.getElementById("u").innerHTML= "Fully Upgrade"
+		x-=500;    document.getElementById("u").innerHTML= "cost:??| Fully Upgrade lv: 4";
 	}
 }
 }
